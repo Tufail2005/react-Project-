257bfe678fff3e70241a1b9f9b73fbe313324125
tufail2005 <tufail1526@gmail.com>
2025-09-03 23:55:44 +0530

    Navbar done

--- netflixClone/src/components/NavBar/Navbar.jsx
+++ netflixClone/src/components/NavBar/Navbar.jsx
@@ -4,13 +4,13 @@ import logo from "../../assets/logo.png"
 import search_icon from "../../assets/search_icon.svg"
 import bell_icon from "../../assets/bell_icon.svg"
 import profile_img from "../../assets/profile_img.png"
-import caret_icon from "../../assets/caret_icon.png"
+import caret_icon from "../../assets/caret_icon.svg"
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       <div className='navbar-left'>
-        <img src={logo} alt ={logo} />
+        <img src={logo} alt ={"logo"} />
         <ul>
           <li>Home</li>
           <li>TV Shows</li>
@@ -21,12 +21,13 @@ const Navbar = () => {
         </ul>
       </div>
       <div className='navbar-right'>
-        <img src={search_icon} alt={icons} />
+        <img src={search_icon} alt={"icons"} />
         <p>Children</p>
-        <img src={bell_icon} alt={bell_icons} />
+        <img src={bell_icon} alt={"icons"} />
         <div className='navbar-profile'>
-            <img src={profile_img} alt={profile_imgt} className='profile' />
+            <img src={profile_img} alt={"profile_img"} className='profile' />
             <img src={caret_icon} alt={caret_icon} />
+            <div className="dropdown">SignOut of Netflix</div>
         </div>
       </div>
     </div>
