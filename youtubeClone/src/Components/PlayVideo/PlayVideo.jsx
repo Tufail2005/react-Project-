53b66e3ec9fbb03c7b5b72589a64c2d2deae4148
tufail2005 <tufail1526@gmail.com>
2025-09-21 01:40:27 +0530

    done

--- youtubeClone/src/Components/PlayVideo/PlayVideo.jsx
+++ youtubeClone/src/Components/PlayVideo/PlayVideo.jsx
@@ -53,7 +53,6 @@ const PlayVideo = () => {
 
   return (
     <div className="play-video">
-      {/* <video src={video1} controls autoPlay muted ></video> */}
       <iframe
         src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
         frameBorder="0"
